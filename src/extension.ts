f2ca3a556ba9837119ec43b9cbfe82091b603a9e
Drew Pereli <drewpereli@gmail.com>
2021-09-20 01:00:59 -0700

    Scroll to region on click

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,34 @@
 import * as vscode from 'vscode';
-import { RegionsProvider } from './regions';
+import getLineAndColumnFromIndex from './get-line-and-column-from-index';
+import { RegionsProvider as RegionTreeProvider } from './region-tree-provider';
 
 export function activate(context: vscode.ExtensionContext) {
-	const regionsProvider = new RegionsProvider();
-	vscode.window.registerTreeDataProvider('regions', regionsProvider);
+	let treeView = vscode.window.createTreeView('regions', {
+		treeDataProvider: new RegionTreeProvider()
+	});
+
+	treeView.onDidChangeSelection(e => {
+		let region = e.selection[0].region;
+
+		let editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			return;
+		}
+
+		let offsetInfo = getLineAndColumnFromIndex(editor.document.getText(), region.startIndex);
+
+		if (offsetInfo === null) {
+			return;
+		}
+
+		let {line, column} = offsetInfo;
+		
+		let startPosition = new vscode.Position(line, column);
+		let endPosition = new vscode.Position(line + 1, column);
+
+		let range = new vscode.Range(startPosition, endPosition);
+
+		editor.revealRange(range, vscode.TextEditorRevealType.AtTop);
+	});
 }
